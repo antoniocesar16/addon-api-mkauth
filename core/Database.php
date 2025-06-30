6b39aebd16152849bc1746770d3290b6bb0f0ed9
Antônio César <antoniocesar16794@gmail.com>
2025-06-30 04:48:43 -0300

    update

--- core/Database.php
+++ core/Database.php
@@ -17,7 +17,7 @@ class Database {
         $this->connection = $conexao;
         
         if (!$this->connection) {
-            throw new Exception('Erro ao conectar com o banco de dados', mysqli_connect_error());
+            throw new Exception('Erro ao conectar com o banco de dados: ', mysqli_connect_error());
         }
     }
     
